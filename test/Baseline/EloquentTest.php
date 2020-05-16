ef04a25119c5f631420fac33a5b387abf6aa1ee3
poing <brian.lavallee@invite-comm.jp>
2020-05-17 01:08:23 +0900

    Added baseline Orchestra tests

---
+++ test/Baseline/EloquentTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Earmark\Test\Baseline;
+
+use Earmark\Test\Baseline\Models\BaseLineAlpha;
+
+class EloquentTest extends UnitAbstract
+{
+    /** @test */
+    public function record_count_test()
+    {
+        factory(BaseLineAlpha::class, 50)->create();
+        $this->assertEquals(BaseLineAlpha::count(), 50);
+    }
+}
---
+++ test/Baseline/database/migrations/2020_04_18_123058_create_base_line_alphas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBaseLineAlphasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('base_line_alphas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('base_line_alphas');
+    }
+}
